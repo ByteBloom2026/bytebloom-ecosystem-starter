42bc5be04d94c0428b95c07e2e84eeb530a2bc28
ibtisam safi <safiibtisam891@gmail.com>
2025-12-09 13:32:37 +0200

    Refactor Mentee data class properties
    Removed default values for team and submissions properties.

--- src/main/kotlin/domain/Mentee.kt
+++ src/main/kotlin/domain/Mentee.kt
@@ -3,6 +3,6 @@ package domain
 data class Mentee (
     val name: String,
     val menteeid:String,
-    var team: Team? = null,
-    val submissions: MutableList<PerformanceSubmission> = mutableListOf()
+    var team: Team? 
+    val submissions: MutableList<PerformanceSubmission> 
 )
