1603d975da400d12525c3a8441818b485846492d
ibtisam safi <safiibtisam891@gmail.com>
2025-12-09 13:35:35 +0200

    Fix formatting and spacing in Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,19 +7,19 @@ import java.io.File
 fun main() {
     println("ByteBloom Academy: Ecosystem Project Starter")
     val theNewMentees = parseMenteeRaw()
-    if (theNewMentees != null) {
-        println("totel mentees : {${theNewMentees.size}}")
+    if (theNewMentees!= null) {
+        println("totel mentees : {${theNewMentees .size}}")
     } else {
         println("no  find data on mentees file")
     }
-    val theNewPerformance = parsePerformanceRaw()
-    if (theNewPerformance != null) {
+    val theNewPerformance  = parsePerformanceRaw()
+    if (theNewPerformance!= null) {
         println("totel preformance : {${theNewPerformance.size}}")
     } else {
         println("no  find data on predormance file")
     }
     val parsedTeams = parseTeamData()
-    if (parsedTeams != null) {
+    if (parsedTeams!= null) {
         println("Total teams parsed : ${parsedTeams.size}")
     } else {
         println("No team data available for parsing .")
