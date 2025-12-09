1e56dd3fa554bdf2bc67ff2ee72abb071101393d
ibtisam safi <safiibtisam891@gmail.com>
2025-12-09 13:48:30 +0200

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,19 +4,19 @@ fun main() {
     val theNewMentees = parseMenteeRaw()
     if (theNewMentees!= null) {
         println("totel menteess : {${theNewMentees.size}}")
-    }else{
+    } else{
         println("no  find data on mentees file")
     }
     val theNewPerformances = parsePerformanceRaw()
     if (theNewPerformances!= null) {
         println("totel preformances : {${theNewPerformances.size}}")
-    }else{
+    } else{
         println("no  find data on predormance file")
     }
     val parsedTeams = parseTeamData()
     if (parsedTeams!= null) {
         println("Total teams parsed : ${parsedTeams.size}")
-    }else{
+    } else{
         println("No team data available for parsing .")
     }
 }
