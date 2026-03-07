151377e9c04ef3e4029951388794cec277ba52d4
Mohammed-Alhams <mohammed1212alhams@gmail.com>
2026-03-07 15:11:13 +0200

    create custom exception

---
+++ src/main/kotlin/domain/model/exception/exceptions.kt
@@ -0,0 +1,9 @@
+package domain.model.exception
+
+class EmptyTeamNameException: Exception()
+
+class InvalidTeamNameLengthException: Exception()
+
+class EmptyMenteeNameException: Exception()
+
+class NotCapitalizedNameException: Exception()
