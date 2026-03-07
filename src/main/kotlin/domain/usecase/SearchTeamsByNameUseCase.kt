b3601f95cefdda012980bdf562f0914b629b51a4
Mohammed-Alhams <mohammed1212alhams@gmail.com>
2026-03-07 14:42:51 +0200

    rename search argument name

--- src/main/kotlin/domain/usecase/SearchTeamsByNameUseCase.kt
+++ src/main/kotlin/domain/usecase/SearchTeamsByNameUseCase.kt
@@ -3,8 +3,10 @@ package domain.usecase
 import data.repository.TeamRepository
 import domain.model.Team
 
-class SearchTeamsByNameUseCase(private val teamRepository: TeamRepository) {
-    operator fun invoke(query: String): List<Team> {
+class SearchTeamsByNameUseCase(
+    private val teamRepository: TeamRepository
+) {
+    operator fun invoke(keyword: String): List<Team> {
         return teamRepository.getAllTeams()
             .filter { it.name.contains(query, ignoreCase = true) }
     }
